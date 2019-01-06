d3fc7b25316f4d96acee2165525545b81fc0a820
Imran Hossain <imran096@gmail.com>
2019-01-07 01:10:13 +0600

    test 2

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const path = require('path')
 const app = express()
 const port = 3000
 
-// app.get('/apis', (req, res) => res.json({name:'Hello World!',done:'yes'}))
+ app.get('/apis', (req, res) => res.json({name:'Hello World!',done:'yes'}))
 app.get('/', (req, res) => res.sendFile(path.join(__dirname+'/index.html')))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
