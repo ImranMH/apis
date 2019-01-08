78be5472ffec1590ddb6e72181b4f3bf062314fd
Imran Hossain <imran096@gmail.com>
2019-01-09 01:53:53 +0600

    test4

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 const express = require('express')
 const path = require('path')
 const app = express()
-const port = 3000
+const port = 5000
 
 // app.engine('html', require('ejs').renderFile);
 // app.set()
 app.set('view engine', 'ejs');
 app.set('views', __dirname + '/views'); 
- app.get('/apis', (req, res) => res.json({name:'Hello World!',done:'yes'}))
+//  app.get('/apis', (req, res) => res.json({name:'Hello World!',done:'yes'}))
 app.get('/', (req, res) => res.render('home'))
 app.get('/login', (req, res) => res.render('login'))
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
